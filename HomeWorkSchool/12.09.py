dd0d9c8a559bd15f18751236b8a426d8c736ce8b
Ahmeddedu <156914766+Ahmeddedu@users.noreply.github.com>
2025-09-12 13:56:42 +0300

    task 2

---
+++ HomeWorkSchool/12.09.py
@@ -0,0 +1,7 @@
+a = float(input("Type first numsb"))# float более точный то инт не всягда удобен в делении ;)
+b = float(input("Type second number"))
+
+if b == 0:
+    print("Are you stupid to delit to zero")
+else:
+    print("Resut - " , a/b)
